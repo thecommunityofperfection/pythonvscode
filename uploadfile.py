115adf01b1bcbfe7caa696526549288f974da1d5
Qwarty@2 <highwaymanlego@gmail.com>
2024-10-08 18:17:28 -0500

    save

--- uploadfile.py
+++ uploadfile.py
@@ -5,8 +5,8 @@ import pandas as pd
 #Allow user to choose columns to plot
 #Alow user to choose arithmetic operation(Sum, Average)
 #Allow user to upload and view an image
-
-menu = st.sidebar.selectboxgit init("Choose an option:", ["Upload CSV", "Upload Image"])
+st.set_page_config(page_title="Upload Files", page_icon="⬆️")
+menu = st.sidebar.selectbox("Choose an option:", ["Upload CSV and edit", "Upload Image", "Upload Video", "Upload Audio"])
 
 if menu == "Upload CSV":
     uploadcsv = st.file_uploader("Upload CSV File:", type="csv")
@@ -23,3 +23,24 @@ if menu == "Upload Image":
 
     if uploadimage:
         st.image(uploadimage,use_column_width=True)
+
+
+    
+if menu == "Upload Audio":
+    uploadaudio = st.file_uploader("Upload Audio File:", type=["mp3", "wav"])
+
+    if uploadaudio:
+        with st.expander("View File"):
+            st.audio(uploadaudio,format='audio/mp3')
+
+
+        
+if menu == "Upload Video":
+    st.subheader("Upload Youtube Video Link")
+    youtubelink = st.text_input("Paste Youtube Video Link")
+    if st.button("Play Youtube Video"):
+        if youtubelink:
+            try:
+                st.video(youtubelink)
+            except:
+                st.error("Sorry cannot play this video link")
