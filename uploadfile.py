577acd605763cef0504c067c17db5c1a9cd6e2fe
Qwarty@2 <highwaymanlego@gmail.com>
2024-10-10 18:31:27 -0500

    save

--- uploadfile.py
+++ uploadfile.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 #Allow user to upload a csv file
 #Allow user to view chart
 #Allow user to choose columns to plot
@@ -12,12 +13,31 @@ if menu == "Upload CSV and edit":
     uploadcsv = st.file_uploader("Upload CSV File:", type="csv")
 
     if uploadcsv:
-        csvlink = pd.read_csv(uploadcsv)
+        readcsv = pd.read_csv(uploadcsv)
         with st.expander("View File"):
-            st.table(csvlink)
+            st.table(readcsv)
+    
+    readcsvcolumns = readcsv.columns #READ ALL COLUMNS IN CSV FILE
 
+    selectcolumns = st.multiselect("Choose columns to plot", readcsvcolumns)
+    col1, col2 = st.columns(2)
+    with col1:
+        selectchart = st.radio("Choose Chart to plot",["Bar chart", "Pie Chart"], horizontal = True)
+    with col2:
+        selectoperator = st.radio("Choose Stats Operator",["Average", "Sum", "Count"], horizontal = True)
 
-
+    if selectcolumns:
+        if selectoperator == "Average":
+            ave_op = readcsv[selectcolumns].mean().reset_index()
+            st.table(ave_op)
+            if selectchart == "Bar Chart":
+                barchart = px.bar(ave_op, x = "index", y=0, labels = {"index": "Subject","0":"Average"})
+                st.plotly_chart(ave_op, x="index", y=0)
+        
+        elif selectchart == "Pie Chart":
+            piechart = px.pie(ave_op, names = "index", values = 0, labels = {"index": "Subject","0":"Average"})
+            st.plotly_chart(piechart)
+               
 if menu == "Upload Image":
     uploadimage = st.file_uploader("Upload Image File:", type=["jpg", "jpeg", "png"])
 
