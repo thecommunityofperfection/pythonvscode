571dcef5d042ec886c97e9e5ce41fc8eff9c0e84
Isaac Kroeker <isaackroeker@outlook.com>
2024-12-07 21:34:54 -0600

    Organized code

--- churchage.py
+++ churchage.py
@@ -1,32 +1,62 @@
 import streamlit as st 
 import pandas as pd
 
-csvlink = pd.read_csv("ages.csv")
+ADMIN_PASSWORD = "testpassword"
+CSV_FILE_PATH = "ages.csv"
+
+csvfile = pd.read_csv(CSV_FILE_PATH)
 menu = st.sidebar.selectbox("Menu", ["Submit data", "Database"])
-if menu == "Database":
-    adminpass =  st.sidebar.text_input("Enter password: ", type="password")
-    if adminpass == "testpassword":
-        st.table(csvlink)
-else:
-    user = st.text_input("Choose a username:")
+
+# Define age groups
+ages = {
+    "Kids": [3, 12],
+    "Teens": [13, 19],
+    "Youth": [20, 35],
+    "Adults": [36, 63],
+}
+
+def get_age_group(age):
+    for group, (min_age, max_age) in ages.items():
+        if min_age <= age <= max_age:
+            return group
+    return "Elders"
+
+def admin_page():
+    st.title("Admin Page")
+    password = st.text_input("Enter admin password: ", type="password")
+    if password == ADMIN_PASSWORD:
+        st.table(csvfile)
+
+def user_page():
+    username = st.text_input("Choose a username:")
     password = st.text_input("Choose a password:", type="password")
     age = st.number_input("How old are you?",0)
     name = st.text_input("What is your name?")
     gender = st.radio("Gender:", ["Male", "Female"])
-    if age > 2 and age < 13:
-        group = "Kids"
-    elif age > 12 and age < 20:
-        group = "Teens"
-    elif age > 19 and age < 36:
-        group = "Youth"
-    elif age > 36 and age < 64:
-        group = "Adults"
-    else:
-        group = "Elders"
+
+    group = get_age_group(age)
+
     if st.button("Sign up"):
-        agedict = {"Username":[user],"Password":[password],"Name":[name],"Age":[age],"Gender":[gender], "Group":[group]}
-        agetable = pd.DataFrame(agedict)
-        jointables = pd.concat([csvlink, agetable], ignore_index=True)
-        jointables.to_csv("ages.csv", index=False)
-        st.write("Your group is:", group)
-        st.success("Submission complete!")
+        sign_up(username, password, name, age, gender, group)
+
+def sign_up(username, password, name, age, gender, group):
+    user_info = {
+        "Username":[username],
+        "Password":[password],
+        "Name":[name],
+        "Age":[age],
+        "Gender":[gender],
+        "Group":[group]
+    }
+    user_table = pd.DataFrame(user_info)
+
+    updated_table = pd.concat([csvfile, user_table], ignore_index=True)
+    updated_table.to_csv(CSV_FILE_PATH, index=False)
+
+    st.write("Your group is:", group)
+    st.success("Submission complete!")
+    
+if menu == "Submit data":
+    user_page()
+elif menu == "Database":
+    admin_page()
