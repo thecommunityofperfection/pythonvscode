54c33a19520724f94d95929e9627129e67bce9b3
Isaac Kroeker <isaackroeker@outlook.com>
2024-12-07 22:24:42 -0600

    Organized code, added unused values

--- doctor.py
+++ doctor.py
@@ -1,50 +1,76 @@
 import streamlit as st
 import pandas as pd
 
+ADMIN_PASSWORD = "testpassword"
+CSV_FILE_PATH = "appointment.csv"
+
 st.set_page_config(page_title="Book An Appointment", page_icon="🧑‍⚕️")
 menu = st.sidebar.selectbox("Menu", ["Book Appointment", "Database"])
-csvlink = pd.read_csv("appointment.csv")
-if menu == "Database":
-    adminpass =  st.sidebar.text_input("Enter admin password: ", type="password")
-    #user = st.text_input("Input Username: ")
-    #password = st.text_input("Input Password: ", type="password")
-    if adminpass == "testpassword":
-        st.table(csvlink)
-elif menu == "Book Appointment":
+csvfile = pd.read_csv(CSV_FILE_PATH)
+
+user_data = {
+    "FirstName": None,
+    "LastName": None,
+    "Gender": None,
+    "Birthday": None,
+    "Occupation": None,
+    "Phone": None,
+    "Street1": None,
+    "Street2": None,
+    "Province": None,
+    "City": None,
+    "State": None,
+}
+
+
+
+def admin_page():
+    st.title("Admin Page")
+    password = st.text_input("Enter admin password: ", type="password")
+    if password == ADMIN_PASSWORD:
+        st.table(csvfile)
+
+def user_page():
+    global user_data
+
     st.subheader("Doctor Appointment Request Form")
     st.write("Fill the form below and we will get back to you soon for more updates and plan your appointment.")
 
-    left, right, = st.columns(2)
+    left_column, right_column, = st.columns(2)
 
-    with left:
-        firstname = st.text_input("First Name:")
-    with right:
-        lastname = st.text_input("Last Name:")
-    with left:
-        birthday = st.date_input("Choose your birthday:") 
-    with right:
-        occupation = st.text_input("Enter your occupation:")
-    with left:
+    with left_column:
+        user_data["FirstName"] = st.text_input("First Name:")
+        user_data["Birthday"] = st.date_input("Choose your birthday:") 
+
         st.write("")
-        gender = st.radio("What is your gender", ["Male", "Female"], horizontal=True)
-    with right:
-        phone = st.text_input("Phone number")
 
-    street1 = st.text_input("Street Address 1:")
-    street2 = st.text_input("Street Address 2:")
+        user_data["Gender"] = st.radio("What is your gender", ["Male", "Female"], horizontal=True)
+    with right_column:
+        user_data["LastName"] = st.text_input("Last Name:")
+        user_data["Occupation"] = st.text_input("Enter your occupation:")
+        user_data["Phone"] = st.text_input("Phone number")
+
+
+    user_data["Street1"] = st.text_input("Street Address 1:")
+    user_data["Street2"] = st.text_input("Street Address 2:")
 
     left2, right2 = st.columns(2)
 
     with left2:
-        st.text_input("City:")
+        user_data["City"] = st.text_input("City:")
+        request_button = st.button("Request Appointment")
     with right2:
-        state = st.text_input("State/Province")
-    with left2: 
-        if st.button("Request Appointment"):
-            doctordict = {"FirstName":[firstname],"LastName":[lastname],"Gender":[gender],"Birthday":[birthday],"Occupation":[occupation],"Phone":[phone],"Street1":[street1],"Street1":[street2],"Province":[state]}
-            doctortable = pd.DataFrame(doctordict)
-            #st.write(doctordict)
-            #st.write(doctortable)
-            jointables = pd.concat([csvlink, doctortable], ignore_index=True)
-            jointables.to_csv("appointment.csv", index=False)
-            st.success("Submission complete!")
+        user_data["State"] = st.text_input("State/Province")
+
+    if request_button:
+        user_table = pd.DataFrame([user_data])
+
+        jointables = pd.concat([csvfile, user_table], ignore_index=True)
+        jointables.to_csv(CSV_FILE_PATH, index=False)
+        st.success("Submission complete!")
+
+
+if menu == "Database":
+    admin_page()
+elif menu == "Book Appointment":
+    user_page()
