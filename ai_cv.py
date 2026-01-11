240edd5b6bc81e869d6a2f0b57c0ffef43293403
Qwarty@2 <highwaymanlego@gmail.com>
2026-01-11 14:56:31 -0600

    save

--- ai_cv.py
+++ ai_cv.py
@@ -5,6 +5,10 @@ import base64
 
 
 st.set_page_config(layout = "wide", page_title = "Sam's AI CV generator", page_icon = "")
+
+if "generate" not in st.session_state:
+    st.session_state.generate = 0
+    st.rerun()
 # pdf.ln(3)
 #---------------------CONFIG-------------------------
 api_key = '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'#API PROGRAM INTERFACE
@@ -182,63 +186,64 @@ with st.sidebar:
 
     #---------------------------------------------------------
     #st.session_state.generate =  st.checkbox("Generate AI CV")
-if st.button("Generate AI CV"):
-    st.session_state.generate = 1
+    if st.session_state.generate == 0:
+        if st.button("Generate AI CV"):
+            st.session_state.generate += 1
 
-        
+    
 
 
 
-    #if st.session_state.generate:
-    if st.session_state.generate == 1:
-        with st.spinner("Creating your CV"):
-            if name and address and skills and education and experience:
-                st.session_state.generate = 100
-                st.session_state.pro_response = ask_ai(summary)
-                st.session_state.pro_edit = st.text_area(value = st.session_state.pro_response, height = 150, label = "Professional Summary   :blue[Click to edit]")
-                st.session_state.skills_response = ask_ai(skills_summary)
-                st.session_state.skills_edit = st.text_area(value = st.session_state.skills_response, height = 150, label = "Skills response   :blue[Click to edit]")
-                st.session_state.work_response = ask_ai(work_summary)
-                st.session_state.work_edit = st.text_area(value = st.session_state.work_response, height = 150, label = "Work response   :blue[Click to edit]")
-                st.session_state.education_response = ask_ai(education_summary)
-                st.session_state.education_edit = st.text_area(value = st.session_state.education_response, height = 150, label = "Education response   :blue[Click to edit]")
-                st.session_state.reference_response = ask_ai(reference_summary)
-                st.session_state.reference_edit = st.text_area(value = st.session_state.reference_response, height = 150, label = "Reference response   :blue[Click to edit]")
-                if st.button("Save edits:"):
-                    st.session_state.pro_response = st.session_state.pro_edit
-                    st.session_state.skills_response = st.session_state.skills_edit
-                    st.session_state.work_response = st.session_state.work_edit
-                    st.session_state.education_response = st.session_state.education_edit
-                    st.session_state.reference_response = st.session_state.reference_edit
-                    st.rerun()
-                #if st.button('Show pdf'):
-                pdf_func = generate_pdf()
-                with open(pdf_func, "rb") as readtext:
-                    pdf_data = readtext.read()
-                pdf_base64 = base64.b64encode(pdf_data).decode('utf-8')
-                #pdf_embed = f'<embed src= "data:application/pdf;base64, {pdf_base64}" type= "application/pdf" width="100%" height="600px" />'
-                #st.markdown(pdf_embed,unsafe_allow_html=True)
-                st.download_button(label = "Download PDF", data = pdf_data, file_name = name + "_cv.pdf", mime = "application/pdf")
+#if st.session_state.generate:
+if st.session_state.generate == 1:
+    with st.spinner("Creating your CV"):
+        if name and address and skills and education and experience:
+            st.session_state.generate = 100
+            st.session_state.pro_response = ask_ai(summary)
+            st.session_state.pro_edit = st.text_area(value = st.session_state.pro_response, height = 150, label = "Professional Summary   :blue[Click to edit]")
+            st.session_state.skills_response = ask_ai(skills_summary)
+            st.session_state.skills_edit = st.text_area(value = st.session_state.skills_response, height = 150, label = "Skills response   :blue[Click to edit]")
+            st.session_state.work_response = ask_ai(work_summary)
+            st.session_state.work_edit = st.text_area(value = st.session_state.work_response, height = 150, label = "Work response   :blue[Click to edit]")
+            st.session_state.education_response = ask_ai(education_summary)
+            st.session_state.education_edit = st.text_area(value = st.session_state.education_response, height = 150, label = "Education response   :blue[Click to edit]")
+            st.session_state.reference_response = ask_ai(reference_summary)
+            st.session_state.reference_edit = st.text_area(value = st.session_state.reference_response, height = 150, label = "Reference response   :blue[Click to edit]")
+            if st.button("Save edits:"):
+                st.session_state.pro_response = st.session_state.pro_edit
+                st.session_state.skills_response = st.session_state.skills_edit
+                st.session_state.work_response = st.session_state.work_edit
+                st.session_state.education_response = st.session_state.education_edit
+                st.session_state.reference_response = st.session_state.reference_edit
+                st.rerun()
+            #if st.button('Show pdf'):
+            pdf_func = generate_pdf()
+            with open(pdf_func, "rb") as readtext:
+                pdf_data = readtext.read()
+            pdf_base64 = base64.b64encode(pdf_data).decode('utf-8')
+            #pdf_embed = f'<embed src= "data:application/pdf;base64, {pdf_base64}" type= "application/pdf" width="100%" height="600px" />'
+            #st.markdown(pdf_embed,unsafe_allow_html=True)
+            st.download_button(label = "Download PDF", data = pdf_data, file_name = name + "_cv.pdf", mime = "application/pdf")
 
-                
-            else:
-                st.session_state.generate = 0
-                st.error("Please fill out all the required info")
-    if st.session_state.generate == 100:
-        st.session_state.pro_edit = st.text_area(value = st.session_state.pro_response, height = 150, label = "Professional Summary   :blue[Click to edit]")
-        st.session_state.skills_edit = st.text_area(value = st.session_state.skills_response, height = 150, label = "Skills response   :blue[Click to edit]")
-        st.session_state.work_edit = st.text_area(value = st.session_state.work_response, height = 150, label = "Work response   :blue[Click to edit]")
-        st.session_state.education_edit = st.text_area(value = st.session_state.education_response, height = 150, label = "Education response   :blue[Click to edit]")
-        st.session_state.reference_edit = st.text_area(value = st.session_state.reference_response, height = 150, label = "Reference response   :blue[Click to edit]")
-        if st.button("Save edits:"):
-            st.session_state.pro_response = st.session_state.pro_edit
-            st.session_state.skills_response = st.session_state.skills_edit
-            st.session_state.work_response = st.session_state.work_edit
-            st.session_state.education_response = st.session_state.education_edit
-            st.session_state.reference_response = st.session_state.reference_edit
-            st.rerun()
-        pdf_func = generate_pdf()
-        with open(pdf_func, "rb") as readtext:
-            pdf_data = readtext.read()
-        pdf_base64 = base64.b64encode(pdf_data).decode('utf-8')
-        st.download_button(label = "Download PDF", data = pdf_data, file_name = name + "_cv.pdf", mime = "application/pdf")
+            
+        else:
+            st.session_state.generate = 0
+            st.error("Please fill out all the required info")
+if st.session_state.generate == 100:
+    st.session_state.pro_edit = st.text_area(value = st.session_state.pro_response, height = 150, label = "Professional Summary   :blue[Click to edit]")
+    st.session_state.skills_edit = st.text_area(value = st.session_state.skills_response, height = 150, label = "Skills response   :blue[Click to edit]")
+    st.session_state.work_edit = st.text_area(value = st.session_state.work_response, height = 150, label = "Work response   :blue[Click to edit]")
+    st.session_state.education_edit = st.text_area(value = st.session_state.education_response, height = 150, label = "Education response   :blue[Click to edit]")
+    st.session_state.reference_edit = st.text_area(value = st.session_state.reference_response, height = 150, label = "Reference response   :blue[Click to edit]")
+    if st.button("Save edits:"):
+        st.session_state.pro_response = st.session_state.pro_edit
+        st.session_state.skills_response = st.session_state.skills_edit
+        st.session_state.work_response = st.session_state.work_edit
+        st.session_state.education_response = st.session_state.education_edit
+        st.session_state.reference_response = st.session_state.reference_edit
+        st.rerun()
+    pdf_func = generate_pdf()
+    with open(pdf_func, "rb") as readtext:
+        pdf_data = readtext.read()
+    pdf_base64 = base64.b64encode(pdf_data).decode('utf-8')
+    st.download_button(label = "Download PDF", data = pdf_data, file_name = name + "_cv.pdf", mime = "application/pdf")
